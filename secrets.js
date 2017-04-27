fdd805a9cc019d42c00ff0d776f74b4c23b0dde9
Christopher Gupta <gupta.christopher@gmail.com>
2017-04-27 14:28:32 -0400

    no secrets

--- secrets.js
+++ secrets.js
@@ -1,8 +1,3 @@
-module.exports = { Dropbox: {
-    CLIENT_ID: 'v9etv1wqzqiukph',
-    CLIENT_SECRET: '5uorsxuj4of0ijm',
-    ACCESS_TOKEN: '88b-3EFkGZAAAAAAAAALy72ZdadAVrPYkKbegoqrpArkbEj8qykaY0ePJNQcKBTR'
-  }
-}
+
 
 
