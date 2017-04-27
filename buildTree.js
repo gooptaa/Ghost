398476a90ce4d43766395e78b66ce9385ef65943
Christopher Gupta <gupta.christopher@gmail.com>
2017-04-27 15:46:45 -0400

    grabbing files

---
+++ buildTree.js
@@ -0,0 +1,14 @@
+const Dropbox = require('dropbox');
+const fs = require('fs')
+
+let accessToken = require('./secrets').Dropbox.ACCESS_TOKEN
+
+var dbx = new Dropbox({ accessToken: accessToken });
+
+dbx.filesListFolder({path: ''})
+  .then(function(response) {
+    console.log(response);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
--- index.js
+++ index.js
@@ -13,10 +13,13 @@ dbx.filesListFolder({path: ''})
     console.log(error);
   });
 
-dbx.filesDownload({path: '/screenshot_2017-01-04-19-46-24.png'})
-  .then(function(response){
-    console.log(response)
-  })
-  .catch(function(error){
-    console.log(error)
-  })
+// dbx.filesDownload({path: '/screenshot_2017-01-04-19-46-24.png'})
+//   .then(function(response){
+//     fs.writeFile(`./for_tests/${response.name}`, response.fileBinary, 'binary', (err) => {
+//       if (err) throw err
+//     })
+//     console.log(Object.keys(response))
+//   })
+//   .catch(function(error){
+//     console.log(error)
+//   })
